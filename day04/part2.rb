bf11803f6bb6d979fc1def48cc04ea8713893bba
nipinium <nipinium@gmail.com>
2018-12-01 23:16:33 +0700

    Day 04

---
+++ day04/part2.rb
@@ -0,0 +1,10 @@
+require 'digest'
+
+def mining(s, i = 0)
+  i += 1 while !Digest::MD5.hexdigest(s + i.to_s).start_with?('000000')
+  i
+end
+
+# puts mining('abcdef')
+# puts mining('pqrstuv')
+puts mining('yzbqklnj')
