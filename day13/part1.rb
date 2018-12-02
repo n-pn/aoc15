7f31a7e51469d9857a8b7300950f7c75925df11b
nipinium <nipinium@gmail.com>
2018-12-02 16:17:01 +0700

    Day 13

---
+++ day13/part1.rb
@@ -0,0 +1,27 @@
+@hp = Hash.new{|h, k| h[k] = Hash.new(0)}
+@re = /^(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+).$/
+
+File.readlines('./input').each do |line|
+  person, react, value, target = line.match(@re).captures
+  value = value.to_i
+  value = -value if react == 'lose'
+  @hp[person][target] = value
+end
+
+people = @hp.keys
+size = people.size
+
+max_happy = 0
+
+people.permutation do |permu|
+  happy = 0
+  permu.each_with_index do |person, index|
+    happy += @hp[person][permu[(index - 1) % size]]
+    happy += @hp[person][permu[(index + 1) % size]]
+  end
+
+  max_happy = happy if happy > max_happy
+end
+
+puts max_happy
+
